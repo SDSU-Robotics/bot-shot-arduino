d49360187cb6614dce51debbbac1026137389831
Joel Kocer <joel.kocer@jacks.sdstate.edu>
2019-04-04 20:31:53 -0500

    removed the miserable IMU communication stuff

--- bot-shot-arduino.ino
+++ bot-shot-arduino.ino
@@ -1,54 +1,4 @@
-/* MPU9250 Basic Example Code
-  by: Kris Winer
-  date: April 1, 2014
-  license: Beerware - Use this code however you'd like. If you
-  find it useful you can buy me a beer some time.
-  Modified by Brent Wilkins July 19, 2016
-
-  Demonstrate basic MPU-9250 functionality including parameterizing the register
-  addresses, initializing the sensor, getting properly scaled accelerometer,
-  gyroscope, and magnetometer data out. Added display functions to allow display
-  to on breadboard monitor. Addition of 9 DoF sensor fusion using open source
-  Madgwick and Mahony filter algorithms. Sketch runs on the 3.3 V 8 MHz Pro Mini
-  and the Teensy 3.1.
-
-  SDA and SCL should have external pull-up resistors (to 3.3V).
-  10k resistors are on the EMSENSR-9250 breakout board.
-
-  Hardware setup:
-  MPU9250 Breakout --------- Arduino
-  VDD ----------------------- 3.3V
-  SDA ----------------------- SDA
-  SCL ----------------------- SCL
-  GND ----------------------- GND
-*/
-
-#include "quaternionFilters.h"
-#include "MPU9250.h"
 #include <Servo.h>
-
-#define AHRS true         // Set to false for basic data read
-
-const int NUM_READINGS = 10;
-
-float IMUReadings[NUM_READINGS] = {0};
-int readIndex = 0;
-float total = 0;
-float IMUAverage = 0;
-int sum = 0;
-
-
-
-// Pin definitions
-int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
-
-#define I2Cclock 400000
-#define I2Cport Wire
-#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0   // Use either this line or the next to select which I2C address your device is using
-//#define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
-
-
-MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
 
 Servo myservo;
 
@@ -56,176 +6,28 @@ int pos = 0;
 
 void setup()
 {
-  Wire.begin();
   // TWBR = 12;  // 400 kbit/sec I2C speed
   Serial.begin(115200);
 
   myservo.attach(9);
 
-  while (!Serial) {};
-
-  // Set up the interrupt pin, its set as active high, push-pull
-  pinMode(intPin, INPUT);
-  digitalWrite(intPin, LOW);
-
-  // Read the WHO_AM_I register, this is a good test of communication
-  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-
-  if (c == 0x71) // WHO_AM_I should always be 0x71
-  {
-    // Start by performing self test and reporting values
-    myIMU.MPU9250SelfTest(myIMU.selfTest);
-
-    // Calibrate gyro and accelerometers, load biases in bias registers
-    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
-
-    myIMU.initMPU9250();
-    // Initialize device for active mode read of acclerometer, gyroscope, and
-    // temperature
-
-    // Read the WHO_AM_I register of the magnetometer, this is a good test of
-    // communication
-    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
-
-    if (d != 0x48)
-    {
-      // Communication failed, stop here
-      Serial.flush();
-      abort();
-    }
-
-    // Get magnetometer calibration from AK8963 ROM
-    myIMU.initAK8963(myIMU.factoryMagCalibration);
-    // Initialize device for active mode read of magnetometer
-    // Get sensor resolutions, only need to do this once
-    myIMU.getAres();
-    myIMU.getGres();
-    myIMU.getMres();
-
-    // The next call delays for 4 seconds, and then records about 15 seconds of
-    // data to calculate bias and scale.
-    //    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
-
-    //    delay(2000); // Add delay to see results before serial spew of data
-
-  }
-
-  // if (c == 0x71)
-  else
-  {
-    Serial.flush();
-    abort();
-  }
 }
 
 void loop()
 {
-  // If intPin goes high, all data registers have new data
-  // On interrupt, check if data ready interrupt
-  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-  {
-    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
-
-    // Now we'll calculate the accleration value into actual g's
-    // This depends on scale being set
-    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
-
-    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
-
-    // Calculate the gyro value into actual degrees per second
-    // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
-
-    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
-
-    // Calculate the magnetometer values in milliGauss
-    // Include factory calibration per data sheet and user environmental
-    // corrections
-    // Get actual magnetometer value, this depends on scale being set
-    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes
-               * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
-    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes
-               * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
-    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes
-               * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
-  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-
-  // Must be called before updating quaternions!
-  myIMU.updateTime();
-
-  // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
-  // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
-  // (+ up) of accelerometer and gyro! We have to make some allowance for this
-  // orientationmismatch in feeding the output to the quaternion filter. For the
-  // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
-  // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
-  // modified to allow any convenient orientation convention. This is ok by
-  // aircraft orientation standards! Pass gyro rate as rad/s
-  MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
-                         myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, myIMU.mz, myIMU.deltat);
-
-
-
-  myIMU.yaw   = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ()
-                              * *(getQ() + 3)), *getQ() * *getQ() + * (getQ() + 1)
-                      * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) - * (getQ() + 3)
-                      * *(getQ() + 3));
-  myIMU.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ()
-                              * *(getQ() + 2)));
-  myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ() + 1) + * (getQ() + 2)
-                              * *(getQ() + 3)), *getQ() * *getQ() - * (getQ() + 1)
-                      * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) + * (getQ() + 3)
-                      * *(getQ() + 3));
-  
-  myIMU.roll *= RAD_TO_DEG;
-  myIMU.pitch *= RAD_TO_DEG;
-  myIMU.yaw   *= RAD_TO_DEG;
-
-  // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
-  // 	8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
-  // - http://www.ngdc.noaa.gov/geomag-web/#declination
-  myIMU.yaw  -= 8.5;
-
-  float IMU0 = myIMU.roll;
-  
   byte incomingByte[2];
   String msg;
 
-    total = total - IMUReadings[readIndex];
-    IMUReadings[readIndex] = IMU0;
-    total = total + IMUReadings[readIndex];
-    readIndex++;
-
-    if(readIndex >= NUM_READINGS)
-    {
-        readIndex = 0;
-    }
-
-    IMUAverage = total / NUM_READINGS;
-
     if (Serial.available() > 0) 
     {
-        incomingByte[1] = Serial.read()-48;
-        if (incomingByte[1] == 0)
-        {
-          msg = String(IMUAverage, 2);
-          Serial.println(msg);
-        }
-        else if (incomingByte[1] == 1)
+        incomingByte[1] = Serial.read() - 48;
+        
+        if ((incomingByte[1]) == 1)
         {
           incomingByte[2] = Serial.read();
           myservo.write(incomingByte[2]-48);              
         }
      }
     
-  
 
-  myIMU.count = millis();
-  myIMU.sumCount = 0;
-  myIMU.sum = 0;
 }
