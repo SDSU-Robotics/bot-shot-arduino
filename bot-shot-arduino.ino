49c8ae56ca89cef4605fe52ce0518df7fce7f8ff
Joel Kocer <joel.kocer@jacks.sdstate.edu>
2019-04-05 05:54:06 -0500

    Added arduino communication with potentiometer for launcher

--- bot-shot-arduino.ino
+++ bot-shot-arduino.ino
@@ -1,18 +1,25 @@
 #include <Servo.h>
+#define NUM_READINGS 10
 
 Servo myservo;
 
 int pos = 0;
 
+int launchPotReadings[NUM_READINGS];
+int readIndex = 0;
+int total = 0;
+int launchPotAverage = 0;
+
+int launchPotPort = A0;
+
 void setup()
 {
-  // TWBR = 12;  // 400 kbit/sec I2C speed
   Serial.begin(115200);
-
   myservo.attach(9);
 
-  while (!Serial) {};
-
+  for (int thisReading = 0; thisReading < NUM_READINGS; thisReading++) {
+    launchPotReadings[thisReading] = 0;
+  }
 }
 
 void loop()
@@ -20,13 +27,28 @@ void loop()
   byte incomingByte[2];
   String msg;
 
+  total = total - launchPotReadings[readIndex];
+  launchPotReadings[readIndex] = analogRead(launchPotPort);
+  total = total + launchPotReadings[readIndex];
+  readIndex = readIndex++;      
+  if (readIndex >= NUM_READINGS) 
+  {
+    readIndex = 0;
+  }
+  launchPotAverage = total / NUM_READINGS;   
+
   if (Serial.available() > 0)
   {
     incomingByte[1] = Serial.read() - 48;
+    if (incomingByte[1] == 0)
+    {
+        msg = String(launchPotAverage, 2);
+        Serial.println(msg);       
+    }
     if (incomingByte[1] == 1)
     {
-      incomingByte[2] = Serial.read();
-      myservo.write(incomingByte[2] - 48);
+        incomingByte[2] = Serial.read();
+        myservo.write(incomingByte[2] - 48);
     }
   }
   delay(20);
